7dc921cf3108b4c310c3cded3a5d0dd70cbdb844
Mina Nagy Zaki <mnzaki@gmail.com>
2020-01-21 13:52:29 +0100

    peering: implement sending

--- peering/connect.go
+++ peering/connect.go
@@ -1,15 +1,25 @@
 package peering
 
 import (
+	"bufio"
+	"encoding/gob"
 	"log"
 	"net"
 )
 
-func Connect(addr string) (*net.Conn, error) {
+type Sender func(v interface{}) error
+
+func Connect(addr string) (Sender, error) {
 	log.Println("Dial " + addr)
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
 		return nil, err
 	}
-	return &conn, nil
+
+	writer := bufio.NewWriter(conn)
+	enc := gob.NewEncoder(writer)
+	return func(v interface{}) error {
+		defer writer.Flush()
+		return enc.Encode(v)
+	}, nil
 }
